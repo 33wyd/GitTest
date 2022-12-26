839eb7ed0936bce42de3e4a28d114ebb02ae0a74
miical <879754743@qq.com>
2022-12-26 18:58:12 +0800

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Jason Liu on 2022/12/26.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
